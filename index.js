2a73be7c194f643aea983d7c72b27ee089faa900
John Zonneveld <jjczon@gmail.com>
2020-07-30 21:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // Write your code here
 let num1 = 24
 let num2 = 5
-let multiply = num1 * num2
+let num3 = 31
+let num4 = 2
+let multiply = num3 * num4
 let random = Math.random()
 let mod = num1 % num2
 let max = Math.max(1, 2, 3, 20)
